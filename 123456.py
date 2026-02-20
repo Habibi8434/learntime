495b9a73462efe22a641f0c6c1641c868ed2a0a0
m_h_habibi_84 <mohamadhadihabibi1384@gmail.com>
2026-02-20 18:21:50 +0330

    Add files via upload

---
+++ 123456.py
@@ -0,0 +1,27 @@
+date_read = int(input())
+learn1 = int(date_read + 1) 
+learn2 = int(date_read + 3)
+learn3 = int(date_read + 6)
+learn4 = int(date_read + 12)
+learn5 = int(date_read + 24)
+
+if learn1 > 30 :
+    print(learn1 - 30)
+else:
+    print(learn1)
+if learn2 > 30 :
+    print(learn2 - 30)
+else:
+    print(learn2)
+if learn3 > 30 :
+    print(learn3 - 30)
+else:
+    print(learn3)
+if learn4 > 30 :
+    print(learn4 - 30)
+else:
+    print(learn4)
+if learn5 > 30 :
+    print(learn5 - 30)
+else:
+    print(learn5)
